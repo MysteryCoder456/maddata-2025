ee2f57d415f3bb7cdbe371db0736b729723c0c06
MysteryCoder456 <rehatbir.singh@gmail.com>
2025-02-22 15:10:09 -0600

    feat: sign in with spotify

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter/animation.dart';
+import 'package:go_router/go_router.dart';
 import 'dart:math';
 
-void main() async {
+final goRouter = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const LoginPage()),
+    GoRoute(path: '/login-success', redirect: (context, state) => '/home'),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => Scaffold(body: Center(child: Text('Home'))),
+    ),
+  ],
+);
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // TODO: put these into .env file
@@ -13,25 +24,27 @@ void main() async {
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54bnJneWN1cnJweHprc2tjZWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyNTMxODQsImV4cCI6MjA1NTgyOTE4NH0.hpK_TYKhLKMNn9ha_cdXFVaXhGaAeHGX6SMevXcdjSw',
   );
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
-    );
+    return MaterialApp.router(routerConfig: goRouter);
   }
 }
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
 
   @override
@@ -49,6 +62,21 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
     super.dispose();
   }
 
+  Future<void> signInWithSpotify() async {
+    // Login with Spotify
+    // FIX: deep link redirect not working
+    bool success = await Supabase.instance.client.auth.signInWithOAuth(
+      OAuthProvider.spotify,
+      //redirectTo: "music-matcher://codeboi.dev/login-success",
+      authScreenLaunchMode: LaunchMode.externalApplication,
+    );
+
+    print("Logged in with Spotify: $success");
+    if (success && context.mounted) {
+      context.go('/login-success');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,7 +87,10 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
             animation: _controller,
             builder: (context, child) {
               return CustomPaint(
-                size: Size(MediaQuery.of(context).size.width, MediaQuery.of(context).size.height),
+                size: Size(
+                  MediaQuery.of(context).size.width,
+                  MediaQuery.of(context).size.height,
+                ),
                 painter: TransverseWavePainter(_controller.value),
               );
             },
@@ -70,34 +101,54 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
               children: [
                 // Padding()
                 Padding(
-                  padding: const EdgeInsets.only(bottom: 20), // Moves text upwards
+                  padding: const EdgeInsets.only(
+                    bottom: 20,
+                  ), // Moves text upwards
                   child: Text(
                     'Music Matcher',
-                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold, color: Colors.white),
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-                
+
                 SizedBox(height: 10),
                 Padding(
                   padding: const EdgeInsets.only(bottom: 100),
                   child: Text(
                     'Connections through music!',
-                    style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 255, 255, 255)),
-                  )
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                  ),
                 ),
                 SizedBox(height: 50),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: signInWithSpotify,
                   style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 255, 255, 255), // Change button color
-                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15), // Adjust size
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(100), // Rounded corners
+                    backgroundColor: const Color.fromARGB(
+                      255,
+                      255,
+                      255,
+                      255,
+                    ), // Change button color
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 40,
+                      vertical: 15,
+                    ), // Adjust size
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        100,
+                      ), // Rounded corners
+                    ),
                   ),
-                ),
-                  child: Text('Login with Spotify',
-                  style: TextStyle(fontSize: 18, color: Colors.black)),
-                  
+                  child: Text(
+                    'Login with Spotify',
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
                 ),
               ],
             ),
@@ -115,25 +166,27 @@ class TransverseWavePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..color = const Color.fromARGB(255, 63, 255, 175).withOpacity(0.9)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 4
-      ..maskFilter = MaskFilter.blur(BlurStyle.outer, 5); 
-      
-    
+    Paint paint =
+        Paint()
+          ..color = const Color.fromARGB(255, 63, 255, 175).withOpacity(0.9)
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 4
+          ..maskFilter = MaskFilter.blur(BlurStyle.outer, 5);
+
     Path path = Path();
     double amplitude = 45;
     double frequency = 0.05;
     double yOffset = size.height / 1.9;
-    
+
     for (double x = 0; x < size.width; x += 5) {
-      double y = amplitude * 
-                 (1 + animationValue) * 
-                 (1.5 * animationValue) * 
-                 (1 - animationValue) * 
-                 (1 + animationValue) * 
-                 sin((x * frequency) + (animationValue * 2 * 3.1416)) + yOffset;
+      double y =
+          amplitude *
+              (1 + animationValue) *
+              (1.5 * animationValue) *
+              (1 - animationValue) *
+              (1 + animationValue) *
+              sin((x * frequency) + (animationValue * 2 * 3.1416)) +
+          yOffset;
       if (x == 0) {
         path.moveTo(x, y);
       } else {
