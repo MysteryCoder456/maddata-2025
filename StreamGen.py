3818668dbed30c7dccd6c3aa754a2c52cde1249a
Vashishth-afk <vashishthshettii@gmail.com>
2025-02-23 08:47:41 -0600

    StreamGen.py

--- StreamGen.py
+++ StreamGen.py
@@ -2,8 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from TrackID import get_track_id
 # Replace these with your actual Spotify API credentials
-client_id = 'f60b15a4cb87492a9009c1578e0948c4'
-client_secret = '4c9621dbea9841c9828974510879d175'
+client_id="d973327e65b04151bb818b08e93be92a"
+client_secret= "996d45793d1f4ab2b97c93c02c9538ed"
 
 # Authenticate with Spotify
 auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
@@ -12,7 +12,9 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 # Function to get the number of streams for a specific track
 def get_track_streams(name):
     track_id = get_track_id(name)
+    
     track = sp.track(track_id)
+
 
     return track['popularity']
 
